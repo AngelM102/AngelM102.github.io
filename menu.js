479a44effe9a3b7322255bb250e804f535c3ba7f
AngelM102 <morillo0210angel@gmail.com>
2021-03-20 12:18:56 -0400

    Mejoras en diseño

--- menu.js
+++ menu.js
@@ -1,8 +1,15 @@
-let menu = document.querySelector('.header-menu');
-let btn = document.querySelector('.menu-btn');
 
-function menuMobile() {
 
-		menu.classList.toggle('active')
+	function menuMobile() {
 
-}
+		let menu = document.querySelector('.header-menu');
+		let btn = document.querySelector('.menu-btn');
+
+			menu.classList.toggle('active')
+
+		let header = document.querySelector('.header');
+
+			header.classList.toggle('static')
+
+
+	}
